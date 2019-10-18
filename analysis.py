a8b313632fd1cc9d46d656e4b5310683b41b09f5
Ash-one <1047141957@qq.com>
2019-10-18 17:04:43 +0800

    add a new 'stopwords.txt' which can be used now

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ def getFrontAndBack(filename:str):
         keyword = filename.replace('.txt', '').replace('/examples/','')
     else:
         file = open(filename+'.txt',encoding='gbk')
-        keyword = filename.replace('/examples/','')
+        keyword = filename.replace('./examples/','')
 
     str_infile = file.read()
     pattern = '([\S]{0,5})'+keyword+'([\S]{0,5})'
@@ -21,19 +21,22 @@ def getFrontAndBack(filename:str):
 def cutforDic(text:list):
     front_dic = {}
     back_dic = {}
+    stopwords  = [w.strip() for w in open('stopwords.txt').readlines()]
     for t in text:
         seg_list1 = jieba.lcut(t[0])
         seg_list2 = jieba.lcut(t[1])
-        for seg in seg_list1:
-            if seg in front_dic:
-                front_dic[seg]+=1
-            else:
-                front_dic[seg]=1
-        for seg in seg_list2:
-            if seg in back_dic:
-                back_dic[seg]+=1
-            else:
-                back_dic[seg]=1
+        for seg in seg_list1 :
+            if seg not in stopwords:
+                if seg in front_dic :
+                    front_dic[seg]+=1
+                elif seg not in front_dic :
+                    front_dic[seg]=1
+        for seg in seg_list2 :
+            if seg not in stopwords:
+                if seg in back_dic:
+                    back_dic[seg]+=1
+                elif seg not in back_dic:
+                    back_dic[seg]=1
 
     print(sorted(front_dic.items(), key=lambda item: item[1], reverse=True))
     print(sorted(back_dic.items(), key=lambda item: item[1], reverse=True))
